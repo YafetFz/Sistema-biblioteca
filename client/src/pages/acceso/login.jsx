06b48ebc3c10ee3424b8c3bd44705acee03606b1
asterki <asterki.dev@proton.me>
2024-09-07 09:31:35 -0600

    Feat: Relevant CDNs and libraries added

--- client/src/pages/acceso/login.jsx
+++ client/src/pages/acceso/login.jsx
@@ -1,7 +1,9 @@
+import "../../styles/acceso/login.css"
+
 export default function App() {
-  return (
-    <main>
-      <p>Esta es la pag de login</p>
-    </main>
-  );
+    return (
+        <main>
+            <p>Esta es la pag de login</p>
+        </main>
+    );
 }
