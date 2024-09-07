d36cecc4d576dab0923d435cb5cf67bfbda9843b
asterki <asterki.dev@proton.me>
2024-09-07 09:39:56 -0600

    Feat: Frontend-backend link established

--- client/src/pages/acceso/login.jsx
+++ client/src/pages/acceso/login.jsx
@@ -1,9 +1,47 @@
-import "../../styles/acceso/login.css"
+import axios from "axios"; // Librería para hacer peticiones HTTP
+import "../../styles/acceso/login.css";
 
-export default function App() {
+export default function Login() {
+    const login = async () => {
+        try {
+            // "http://localhost:3001/login" es la URL del servidor
+            // "/login" es la ruta API que se está llamando
+            // { email: "texto desde el input", password: "texto desde el input" } es el objeto que se está enviando al servidor
+            // response es la respuesta del servidor
+            // axios.post() es una función asíncrona que envía una petición POST al servidor
+            // await es una palabra clave que indica que la función debe esperar a que la petición termine
+            const response = await axios.post("http://127.0.0.1:5000/login", {
+                email: "texto desde el input",
+                password: "texto desde el input",
+            });
+
+            // Enviar la respuesta del servidor a la consola
+            console.log(response);
+        } catch (error) {
+            // Si hubo un error, enviarlo a la consola
+            console.error(error);
+        }
+    };
+
     return (
         <main>
             <p>Esta es la pag de login</p>
+            <br />
+            <br />
+
+            <form>
+                <label htmlFor="email">Correo electrónico</label>
+                <input type="email" id="email" name="email" />
+                <br />
+
+                <label htmlFor="password">Contraseña</label>
+                <input type="password" id="password" name="password" />
+                <br />
+
+                <button type="button" onClick={login}>
+                    Iniciar sesión
+                </button>
+            </form>
         </main>
     );
 }
--- server/index.py
+++ server/index.py
@@ -1,12 +1,24 @@
 # Flask app
 from flask import Flask, request, jsonify   
+from flask_cors import CORS
 
+# Se crea una instancia de Flask, Flask es un objeto que se encarga de manejar las rutas y las peticiones que se hagan al servidor
 app = Flask(__name__)
 
-@app.route('/api', methods=['POST'])
-def api():
-    datos_random = "Hola mundo"
-    return jsonify({'data': datos_random})
+# Añadir CORS, que permite que la aplicación de front-end se comunique con el servidor
+CORS(app)
 
+# @app.route le dice a python que la función debajo de ella es la que se ejecutará cuando se haga una petición a la ruta especificada
+@app.route('/login', methods=['POST'])
+def api(): # Función que se ejecutará cuando se haga una petición a la ruta /api, debe de ser la misma que la que se puso en el decorador
+    # request.get_json() obtiene los datos que se enviaron en la petición, en este caso se espera que se envíe un json
+    data = request.get_json()
+    print(data)
+
+    # jsonify convierte un diccionario en un json
+    datos_random = "Hola mundo"
+    return jsonify({'data': datos_random}) # Se regresa un json con la llave 'data' y el valor de la variable datos_random
+
+# __name__ es una variable que python asigna a los archivos que se están ejecutando, si el archivo se está ejecutando directamente __name__ tendrá el valor de '__main__'
 if __name__ == '__main__':
-    app.run(port=5000)
+    app.run(port=5000) # Se inicia el servidor en el puerto 5000
